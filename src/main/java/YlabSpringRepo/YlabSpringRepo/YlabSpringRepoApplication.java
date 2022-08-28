9b8648e6a5a7ea8d05476071cadb81588a6b7c39
kapich1192 <kapich1192@yandex.ru>
2022-08-28 19:55:35 +0300

    First commit

---
+++ src/main/java/YlabSpringRepo/YlabSpringRepo/YlabSpringRepoApplication.java
@@ -0,0 +1,13 @@
+package YlabSpringRepo.YlabSpringRepo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YlabSpringRepoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YlabSpringRepoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/YlabSpringRepo/YlabSpringRepo/YlabSpringRepoApplicationTests.java
@@ -0,0 +1,13 @@
+package YlabSpringRepo.YlabSpringRepo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YlabSpringRepoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
